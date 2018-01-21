5e99b0a4e24c6b5aac85c0857a3252f7917c502a
nisdis <niszim@gmail.com>
2018-01-21 20:45:03 +0530

    Added routes drawer and a grid view

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,162 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new ButterApp());
 
-class ButterApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() => runApp(new MaterialApp(
+  home: new HomePage(),
+  routes: <String, WidgetBuilder> {
+    "/PageTwo": (BuildContext context) => new PageTwo()
+  }
+));
+
+
+class HomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.red,
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Login"),
+        backgroundColor: Colors.deepOrangeAccent,
+        flexibleSpace: null,
+        leading: new IconButton(
+          icon: new Icon(Icons.code),
+          tooltip: 'This does nothing',
+          onPressed: null,
+        ),
       ),
-      home: new MyHomePage(title: 'Butter'),
+      body: new Container(
+        child: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center ,
+            children: <Widget>[
+              new IconButton (
+              icon: new Icon(Icons.person, color: Colors.lightBlue),
+              iconSize: 70.0 ,
+              onPressed: null ,
+
+            ),
+            new Text(
+              "You can login to butter \n with the below button\n\n\n"  ,
+               style: new TextStyle(
+                 fontWeight: FontWeight.normal,
+                 fontFamily: "Roboto",
+                 fontSize: 15.0
+               )
+
+            ),
+            new FlatButton(
+              onPressed: (){Navigator.of(context).pushNamed("/PageTwo");},
+              color: Colors.green,
+              textColor: Colors.white ,
+              splashColor: Colors.greenAccent,
+              child: const Text('Login'),
+            ),
+            ],
+          )
+        )
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class PageTwo extends StatelessWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
+  Widget build(BuildContext context){
+    var drawer = new Drawer(
+      child: new ListView(
+        padding: const EdgeInsets.all(0.0),
+        children: <Widget>[
+          new DrawerHeader(
+            decoration: new BoxDecoration(
+            color: Colors.orange,
+            border: new Border.all(
+                    color: Colors.orangeAccent,
+                    width: 3.0,
+                  ),
+          ),
+            child: new Center(
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center ,
+                children: <Widget>[
+                  new IconButton (
+                  icon: new Icon(Icons.face, color: Colors.brown),
+                  iconSize: 80.0,
+                  onPressed: null //(){Navigator.of(context).pop();},
+                ),
+                new Text("I am You!")
+                ],
+              )
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+            ),
+          ),
+          const ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text('Home'),
+            selected: true,
+            onTap: null
+          ),
+          const ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            //enabled: false,
+            onTap: null
+          ),
+          const Divider(),
+          new ListTile(
+            leading: const Icon(Icons.power_settings_new),
+            title: const Text('Logout'),
+            onTap: (){Navigator.popAndPushNamed(context, "/");}
+          ),
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+        ],
+      ),
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+
+    );
     return new Scaffold(
+      drawer: drawer,
       appBar: new AppBar(
-        leading: new IconButton(
-          icon: new Icon(Icons.menu),
-          tooltip: 'Navigation menu',
-          onPressed: null,
-        ),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text("Butter App"),
+        backgroundColor: Colors.deepOrangeAccent,
+        //bottom: ,
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: new GridView.count(
+  primary: false,
+  padding: const EdgeInsets.all(20.0),
+  crossAxisSpacing: 10.0,
+  crossAxisCount: 2,
+  children: <Widget>[
+    const Text('He\'d have you all unravel at the'),
+    const Text('Heed not the rabble'),
+    const Text('Sound of screams but the'),
+    const Text('Who scream'),
+    const Text('Revolution is coming...'),
+    const Text('Revolution, they...'),
+  ],
+)
+      // body: new ListView(
+      //   padding:  const EdgeInsets.all(10.0) ,
+      //   children: <Widget>[
+      //     new Text(
+      //       "something nutty\n\n\n"  ,
+      //        style: new TextStyle(
+      //          fontWeight: FontWeight.normal,
+      //          fontFamily: "Roboto",
+      //          fontSize: 15.0
+      //        )),
+      //     new Text(
+      //          "Butter will save lives\n\n\n"  ,
+      //           style: new TextStyle(
+      //             fontWeight: FontWeight.normal,
+      //
+      //             fontSize: 15.0
+      //           )),
+      //
+      //
+      //   ],
+      // )
+
     );
   }
 }
