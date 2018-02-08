fbfc68f8c78806f6d6da29cd300005be772b1d99
nisdis <niszim@gmail.com>
2018-02-08 23:40:23 +0530

    UI changes on the audio tab

--- lib/HomeTabs/Tab3.dart
+++ lib/HomeTabs/Tab3.dart
@@ -120,40 +120,105 @@ class _RecordtabState extends State<Recordtab> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               new Text(widget.title, style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
-              new InkWell(
-                child: new Container(
-                  alignment: FractionalOffset.center,
-                  child: new Text(isRecord ? 'Stop':'Record'),
-                  height: 40.0,
-                  width: 200.0,
-                  color: Colors.blue,
-                ),
-                onTap: (){
-                  if (isRecord){
-                    _stopRecord();
-                  }else{
-                    _startRecord();
-                  }
-                },
-              ),
+              // new InkWell(
+              //   child: new Container(
+              //     alignment: FractionalOffset.center,
+              //     child: new Text(isRecord ? 'Stop':'Record'),
+              //     height: 40.0,
+              //     width: 200.0,
+              //     color: Colors.blue,
+              //   ),
+              //   onTap: (){
+              //     if (isRecord){
+              //       _stopRecord();
+              //     }else{
+              //       _startRecord();
+              //     }
+              //   },
+              // ),
               new Text('recording: ' + recordPosition.toString()),
               new Text('power: ' + recordPower.toString()),
-              new InkWell(
-                child: new Container(
-                  margin: new EdgeInsets.only(top: 40.0),
-                  alignment: FractionalOffset.center,
-                  child: new Text(isPlay ? 'Stop':'Play'),
-                  height: 40.0,
-                  width: 200.0,
-                  color: Colors.blue,
-                ),
-                onTap: (){
+              new IconButton(
+                icon: new Icon( Icons.mic ) ,
+                iconSize: 75.0 ,
+                color: isRecord ? Colors.red: Colors.blueAccent,
+                onPressed: (){
+                          if (isRecord){
+                            _stopRecord();
+                          }else{
+                            _startRecord();
+                          }
+                        },
+              ),
+              // new InkWell(
+              //   child: new Container(
+              //     margin: new EdgeInsets.only(top: 40.0),
+              //     alignment: FractionalOffset.center,
+              //     child: new Text(isPlay ? 'Stop':'Play'),
+              //     height: 40.0,
+              //     width: 200.0,
+              //     color: Colors.blue,
+              //   ),
+              //   onTap: (){
+              //     if (!isRecord && file.length > 0){
+              //       _StartStopPlay();
+              //     }
+              //   },
+              // ),
+              new IconButton(
+                icon: isPlay ? new Icon( Icons.stop ) : new Icon( Icons.play_arrow ) ,
+                iconSize: 75.0 ,
+                color: isPlay ? Colors.red : Colors.green,
+                onPressed: (){
                   if (!isRecord && file.length > 0){
                     _StartStopPlay();
                   }
                 },
               ),
               new Text('playing: ' + playPosition.toString()),
+              new Container( // gray box
+                child: new Center(
+                  child: new Transform(
+                    child: new Container(
+                      child: new Center(
+                        child:  new Container( // red box
+                          decoration: new BoxDecoration(
+                            color: Colors.green[400],
+                            shape: BoxShape.circle,
+                          ),
+                          padding: new EdgeInsets.all(6.0),
+                          width: (recordPower * 2),
+                          height: (recordPower * 2),
+
+                        child:  new Center(
+                            child:  new Container( // red box
+                              decoration: new BoxDecoration(
+                                color: Colors.red[400],
+                                shape: BoxShape.circle,
+                              ),
+                              padding: new EdgeInsets.all(6.0),
+                              width: (recordPower ),
+                              height: (recordPower ),
+                            ),
+
+                          ),
+                        ),
+
+                      ),
+                      width: 140.0,
+                      height: 140.0,
+                      color: Colors.orangeAccent,
+                    ),
+                    alignment: Alignment.center,
+                    transform: new Matrix4.identity()
+                      ..rotateZ( (isRecord ? recordPosition : 0.001 ) * 3.1415927  / 180),
+                  ),
+
+                ),
+                width: 320.0,
+                height: 240.0,
+              //  color: Colors.grey[300],
+              ),
             ],
           ): new Text('No permissions for record'),
     );
